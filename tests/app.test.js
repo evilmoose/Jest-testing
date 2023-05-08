e36f89c4e534b70ad427451fd6e53d8c6aa19d93
Evilmoose <ronnielopez@gmail.com>
2023-05-08 15:15:13 -0700

    Another test.

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,5 +1,12 @@
 const {square} = require('../app.js');
+
 test('square should square a', function () {
     const res = square(3);
     expect(res).toEqual(9);
 });
+
+test('square should square negative numbers', () => {
+    const res = square(-9);
+    expect(res).toEqual(81);
+});
+
